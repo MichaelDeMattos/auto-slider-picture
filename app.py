4124d2f9d3188ea5a8f5edef2fce8a98ebb0f90f
Michael <chelmto3000@gmail.com>
2021-04-19 00:29:19 -0300

    Upload files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 from config import app_config, app_active 
 from flask import Flask, request, render_template
 
@@ -15,9 +16,17 @@ def create_app(config_name):
     app.secret_key = config.SECRET_KEY
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
+    app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
-    @app.route("/")
+    @app.route("/", methods=["GET", "POST"])
     def index():
+        if request.method == "POST":
+            if 'file_post' not in request.files:
+                return 'there is no file1 in form!'
+            post = request.files['file_post']
+            path = os.path.join(app.config['UPLOAD_FOLDER'], post.filename)
+            post.save(path)
+            return path
         return render_template("index.html")
     
     return app
