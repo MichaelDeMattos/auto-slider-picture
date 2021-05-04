00e08d1b48f62f2eb31e811043cfbf9df1c488fe
Michael de Mattos <michael@ok00971.okubomercantil.com.br>
2021-05-04 10:51:06 -0300

    Reload page using no cache

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, request, render_template, flash, redirect, url_for, ses
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand   
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 # DataBase
@@ -34,7 +34,7 @@ def create_app(config_name):
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
- 
+
     # Login Manager
     login_manager = LoginManager()
     login_manager.login_view = "login"
@@ -49,7 +49,7 @@ def create_app(config_name):
     from resources.util import format_datetime, format_text_for_ascci
     from resources.user import ControllerUser
     from resources.post import ControllerPostNews
-    
+
     @login_manager.user_loader
     def load_user(cod_user):
         return UserDB.query.get(int(cod_user))
@@ -61,7 +61,7 @@ def create_app(config_name):
     @app.route("/singin")
     def singin():
         return render_template("singin.html")
-    
+
     @app.route("/singin", methods=["POST"])
     def singin_post():
         email = request.form.get('inp_email')
@@ -70,7 +70,7 @@ def create_app(config_name):
         password = request.form.get('inp_password')
 
         user = ControllerUser().get_user_by_email(email=email)
-        if user:  
+        if user:
             flash('Email address already exists')
             return render_template('singin.html', notify='danger')
 
@@ -81,7 +81,7 @@ def create_app(config_name):
         if new_user["status"] == 500:
             flash('Error in include new user!!!')
             return render_template('singin.html', notify='danger')
-        
+
         return redirect(url_for('login'))
 
     @app.route("/login")
@@ -97,7 +97,7 @@ def create_app(config_name):
         if not user or not check_password_hash(user.password, password):
             flash('Please check your login details and try again.')
             return render_template('login.html', notify='danger')
-            
+
         login_user(user, remember=False)
         return redirect(url_for('admin'))
 
@@ -109,16 +109,16 @@ def create_app(config_name):
         _files = []
         for row in posts:
             _files.append({
-                "id": row.id, 
-                "file_name": row.file_name, 
+                "id": row.id,
+                "file_name": row.file_name,
                 "create_date": format_datetime(row.create_date),
                 "user_name": ControllerUser().get_user_by_id(row.user_id).split()[0],
             })
         return render_template(
-            "admin.html", 
+            "admin.html",
             user=ControllerUser().get_user_by_id(id_user_session),
             files=_files)
-    
+
     @app.route("/admin", methods=["POST"])
     def admin_post():
         file_name = request.form["file_name"]
@@ -133,10 +133,10 @@ def create_app(config_name):
         shutil.copy(file_path, config.BACKUP_FOLDER)
         """ Remove file this dir uploads """
         os.remove(file_path)
-        
+
         if delete["status"] == 500:
             return "Name file if not exists", 404
-        
+
         if delete["status"] == 200:
             return "\nFile deleted sucessfully!!!", 200
 
@@ -149,9 +149,9 @@ def create_app(config_name):
     @app.route("/view", methods=["GET"])
     def view():
         # ControllerDownload().get_download_img()
-        return render_template("view.html", 
+        return render_template("view.html",
             files=ControllerDownload().scrapy_dir_uploads(),
-            refresh_time=len(ControllerDownload().scrapy_dir_uploads() * 35)
+            refresh_time=len(ControllerDownload().scrapy_dir_uploads() * 35000)
         )
 
     @app.route("/push_post")
@@ -176,7 +176,7 @@ def create_app(config_name):
                 format_text_for_ascci(post.filename).replace(" ", "_")
             )
             post.save(path)
-            
+
             new_post = ControllerPostNews().new_post(
                 user_id=session["_user_id"],
                 file_name=post.filename,
@@ -188,9 +188,9 @@ def create_app(config_name):
 
             if new_post["status"] == 409 or new_post["status"] == 500:
                 flash(new_post["error"])
-                return render_template("posts.html", notify="danger") 
+                return render_template("posts.html", notify="danger")
 
-            """ Renderer response """ 
+            """ Renderer response """
             flash("Archive upload sucessfully")
             return render_template("posts.html", notify="primary")
 
